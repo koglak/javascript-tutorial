55e6d85b854e5db21c9ea68c88a26cefe70604c9
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2023-12-25 17:12:37 +0300

    selected navigator link styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,31 +2,38 @@ import { useState } from "react";
 import * as React from 'react';
 import { jsTitle } from "../types/type";
 
-const Navigation: React.FC = () =>{
+const Navigation: React.FC = () => {
 
     const [javascriptTitles, setJavascriptTitles] = useState<jsTitle[]>([])
 
     React.useEffect(() => {
-        setJavascriptTitles(["strings", "numbers", "arrays", "variables", "objects", "data_variables"].map((t,i)=>({"title": "javascript", "subtitle": t, "id": i})))
+        setJavascriptTitles(["strings", "numbers", "arrays", "variables", "objects", "data_variables"].map((t, i) => ({ "title": "javascript", "subtitle": t, "id": i })))
     }, []);
 
-    return ( 
-    <div>
-       <h2>Welcome to Javascript Tutorial</h2>
+    const selected_navigator = {
+        color: "red",
+        fontWeight: "bold"
+    }
 
-       <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm-3 border p-3 m-2">
-                    <h4>Javascript</h4>
-                    <ul>
-                        {
-                            javascriptTitles.map((title, index)=> <li key={index}><a href={"/"+title["subtitle"]}>{title["subtitle"].substring(0,1).toUpperCase() + title["subtitle"].substring(1)}</a></li>)
-                        }
-                    </ul>
+    return (
+        <div>
+            <h2>Welcome to Javascript Tutorial</h2>
+
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-3 border p-3 m-2">
+                        <h4>Javascript</h4>
+                        <ul>
+                            {
+                                javascriptTitles.map((title, index) => <li key={index} style={sessionStorage.getItem("selected") === title["subtitle"] ? selected_navigator : {}}><a href={"/" + title["subtitle"]} onClick={() => sessionStorage.setItem("selected", title["subtitle"])}>
+                                    {title["subtitle"].substring(0, 1).toUpperCase() + title["subtitle"].substring(1)}
+                                </a></li>)
+                            }
+                        </ul>
+                    </div>
                 </div>
             </div>
-       </div>
-    </div> 
+        </div>
     );
 }
 
